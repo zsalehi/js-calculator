52765aa8d2bbbd60ba4231be914b0c3c8280663a
Zaigham Salehi <zsalehi313@gmail.com>
2022-05-06 17:47:24 -0400

    Made display screens work with all digits and operators

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const numberButtons = document.querySelectorAll('[data-number]');
 const operatorButtons = document.querySelectorAll('[data-operator]');
+const equalButton = document.querySelector('.equate');
+const clearButton = document.querySelector('.clear');
+const deleteButton = document.querySelector('.delete');
+const decimalButton = document.querySelector('.decimal');
 const inputDisplay = document.querySelector('.input');
 const outputDisplay = document.querySelector('.output');
 
@@ -12,20 +16,50 @@ numberButtons.forEach((button) =>
     button.addEventListener('click', () => appendNumber(button.textContent))
 )
 
-function appendNumber(number) {
-    inputDisplay.textContent += number
+operatorButtons.forEach((button) => 
+    button.addEventListener('click', () => setCurrentOperation(button.textContent))
+)
+
+clearButton.addEventListener('click', () => clear())
+decimalButton.addEventListener('click', () => appendDecimal())
+
+function appendNumber(e) {
+    // if inputDisplay is 0 or resetScreen is true
+    if (inputDisplay.textContent === '0' || resetScreen) {
+        shouldResetScreen();
+    }
+    inputDisplay.textContent += e;
 }
 
+function shouldResetScreen() {
+    inputDisplay.textContent = '';
+    resetScreen = false;
+}
 
+function clear() {
+    inputDisplay.textContent = '0';
+    outputDisplay.textContent = '';
+    firstOperand = '';
+    lastOperand = '';
+    currentOpertion = null;
+}
 
+function appendDecimal() {
+    if (resetScreen) shouldResetScreen();
+    if (inputDisplay.textContent === '')
+        inputDisplay.textContent = '0';
+    if (inputDisplay.textContent.includes('.')) return;
+    inputDisplay.textContent += '.';
+}
 
+function setCurrentOperation(e) {
+    currentOpertion = e;
+    firstOperand = parseFloat(inputDisplay.textContent);
+    inputDisplay.textContent = '0';
+    outputDisplay.textContent = `${firstOperand} ${currentOpertion} `;     
+    console.log(e);
 
-
-
-
-
-
-
+}
 
 
 
